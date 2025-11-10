755638785f54a0cd2234ad1f1a4e63f0860349d7
Patrick Almeida <patrick.almeida06@gmail.com>
2025-11-10 19:25:23 -0300

    voltou

--- Controller/EmprestimoLivroController.py
+++ Controller/EmprestimoLivroController.py
@@ -59,7 +59,7 @@ class EmprestimoLivroController:
         #(TODO) Arrumar aqui
         print(emprestimo.getCliente())
         if atraso > 0:
-            multa = Multa(str(uuid.uuid4()), emprestimo, emprestimo.getCliente(), atraso * 2.0, StatusMulta.PENDENTE)
+            multa = Multa(str(uuid.uuid4()), atraso * 2.0, emprestimo, emprestimo.getCliente(), StatusMulta.PENDENTE)
             self.__multaController.addMulta(multa)
             emprestimo.getCliente().addMulta(multa)
 
